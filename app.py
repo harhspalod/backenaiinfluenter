6b5c862bdf7ba27765c28b67ccc9ab6c4688f447
harshspalod <your-email@example.com>
2025-07-22 02:09:43 +0530

    sadf

--- app.py
+++ app.py
@@ -691,7 +691,10 @@ def divine_intervention(
 #     return "helo world"
 
 
-if __name__ == "__main__":
-    import uvicorn
+# if __name__ == "__main__":
+#     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))  # Railway gives dynamic PORT
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
